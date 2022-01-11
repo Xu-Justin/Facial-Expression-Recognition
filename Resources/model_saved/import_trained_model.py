c6b833f5777237e72bf3c0b501851bf740bfb9d8
William Justin <williamjustinxu@gmail.com>
2022-01-11 21:46:27 +0700

    add trained model

--- Resources/model_saved/import_trained_model.py
+++ Resources/model_saved/import_trained_model.py
@@ -3,21 +3,13 @@ from google_drive_downloader import GoogleDriveDownloader as gdd
 
 def download_model(model_name, id, unzip=True):
     print('Downloading %s model from %s'%(model_name, id))
-    gdd.download_file_from_google_drive(file_id=id, dest_path=os.path.join(root, 'Model', '%s.zip'%model_name), unzip=unzip)
+    if (os.path.exists('%s/'%model_name)):
+       shutil.rmtree('%s/'%model_name)
+    gdd.download_file_from_google_drive(file_id=id, dest_path='./%s.zip'%(model_name), unzip=unzip, showsize=True)
+    os.remove('./%s.zip'%(model_name))
     print('Successfully downloaded %s model from %s'%(model_name, id))
 
+
 if __name__ == '__main__':
-    trained_model = [
-        {
-            'model_name' : 'v1-resnet-50-fer2013',
-            'id' : None,
-        },
-        {
-            'model_name' : 'v2-resnet-50-personal',
-            'id' : None,
-        }
-    ]
-    
-    for model in trained_model:
-        download_model(model['model_name'], model['id'])
-        shutil.rmtree('%s.zip'%model[model_name])
+    download_model('v1-resnet-50-fer2013', '1cskda0niSBXBo7PpYzYiK8rkz6Ppnf0Y')
+    download_model('v2-resnet-50-personal', '10ZzUgz8hWHwO_pzSzYlVnvq0Mrw2dbw_')
